4e1a013a1e54c11f3faba6513f85b975241867fd
Allwaysupdated <jacksonstar3@gmail.com>
2017-03-15 08:31:03 +0100

    yes
    yes

--- guessthenumber2.rb
+++ guessthenumber2.rb
@@ -1,12 +1,12 @@
 running = true
-
+system "cls"
 puts "Guess the number, enter difficulty 0-50"
 diff = gets.chomp
   answer = rand(diff.to_i) + 1
 attempt = 1
 #answer
 puts "Enter a number between 1 and " + diff.to_s
-puts answer.to_s
+#puts answer.to_s
 input = gets.chomp
 
 
@@ -21,6 +21,7 @@ while running == true
         puts "Number of attempts: " + attempt.to_s
           abort
   elsif input.to_i >  answer.to_i
+    #system "cls"
       puts "To Big"
         attempt += 1
           input = gets.chomp.to_i
